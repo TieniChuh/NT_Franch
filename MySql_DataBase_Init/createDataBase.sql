41f22767055d5c6821f6a327ba2636ba11d708e1
Michael_Chuh <michaeltianyi@163.com>
2017-07-18 16:06:48 +0800

    1

--- MySql_DataBase_Init/createDataBase.sql
+++ MySql_DataBase_Init/createDataBase.sql
@@ -1,1 +1,12 @@
 CREATE SCHEMA `private_schema` ;
+
+--Project DataJPA begin;
+INSERT INTO `private_schema`.`t_userinfo` (`userid`, `password`, `username`) VALUES ('1', '11', 'username1');
+INSERT INTO `private_schema`.`t_userinfo` (`userid`, `password`, `username`) VALUES ('2', '22', 'username2');
+INSERT INTO `private_schema`.`t_userinfo` (`userid`, `password`, `username`) VALUES ('3', '33', 'username3');
+
+INSERT INTO `private_schema`.`t_accountinfo` (`accountId`, `balance`, `userinfo_id`) VALUES ('1', '100', '1');
+INSERT INTO `private_schema`.`t_accountinfo` (`accountId`, `balance`, `userinfo_id`) VALUES ('2', '200', '2');
+INSERT INTO `private_schema`.`t_accountinfo` (`accountId`, `balance`, `userinfo_id`) VALUES ('3', '300', '3');
+
+--Project DataJPA end;
